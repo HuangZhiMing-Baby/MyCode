a578f11b7e7ec3e0eae4397dcb537b77924198eb
HZM_Code <lovecoding@hzm.com>
2014-11-06 21:29:05 +0800

    the second times

---
+++ src/com/example/adapter/ListAdapter.java
@@ -0,0 +1,87 @@
+package com.example.adapter;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.example.hztravelapp.R;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListAdapter extends BaseAdapter {
+	
+	private Context context;
+	private List<Map<String, Object>> list;
+
+	public ListAdapter(Context context, List<Map<String, Object>> list) {
+		super();
+		this.context = context;
+		this.list = list;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		ViewHolder holder;
+		if (convertView==null){
+			convertView=LayoutInflater.from(context).inflate(R.layout.travel_items_layout, null);
+			holder=new ViewHolder();
+			holder.travel_imageview=(ImageView) convertView.findViewById(R.id.travel_imageview);
+			holder.travel_title=(TextView) convertView.findViewById(R.id.travel_title);
+			holder.travel_distance=(TextView) convertView.findViewById(R.id.travel_distance);
+			holder.travel_summary=(TextView) convertView.findViewById(R.id.travel_summary);
+			holder.travel_price=(TextView) convertView.findViewById(R.id.travel_price);
+			holder.travel_oldprice=(TextView) convertView.findViewById(R.id.travel_oldprice);
+			holder.travel_grade=(TextView) convertView.findViewById(R.id.travel_grade);			
+			convertView.setTag(holder);
+		}else{
+			holder=(ViewHolder) convertView.getTag();
+		}
+		
+		holder.travel_imageview.setImageDrawable(context.getResources().getDrawable((Integer) list.get(position).get("img")));
+		holder.travel_title.setText((String)list.get(position).get("title"));
+		holder.travel_distance.setText((String)list.get(position).get("distance"));
+		holder.travel_summary.setText((String)list.get(position).get("summary"));
+		holder.travel_price.setText((String)list.get(position).get("price"));
+		holder.travel_oldprice.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+		holder.travel_oldprice.setText((String)list.get(position).get("oldprice"));
+		holder.travel_grade.setText((String)list.get(position).get("grade"));
+		return convertView;
+	}
+
+	public class ViewHolder{
+		ImageView travel_imageview;
+		TextView travel_title;
+		TextView travel_distance;
+		TextView travel_summary;
+		TextView travel_price;
+		TextView travel_oldprice;
+		TextView travel_grade;
+	}
+}
---
+++ src/com/example/hztravelapp/TravelActivity.java
@@ -0,0 +1,141 @@
+package com.example.hztravelapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.example.adapter.ListAdapter;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.format.DateUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+public class TravelActivity extends ActionBarActivity {
+	
+	private Spinner mSpinner;
+	private PullToRefreshListView mPullToRefreshListView;
+	private List<Map<String, Object>> mList=new ArrayList<Map<String,Object>>();
+	LinkedList<String> list=new LinkedList<String>();
+	private String[] citys=new String[]{
+			"全城",
+			"热门商区",
+			"惠城区",
+			"惠阳区",
+			"惠东区",
+			"龙门县",
+			"博罗县",
+	};	
+	private ArrayAdapter<String> adapter;
+	private ListAdapter mListAdapter;
+	private Map<String, Object> map;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.travel_layout);
+		for (String city :citys){
+			list.add(city);
+		}
+		
+		mSpinner=(Spinner) findViewById(R.id.allcity);
+		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+		mSpinner.setAdapter(adapter);
+		
+		
+		mPullToRefreshListView=(PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+		mListAdapter=new ListAdapter(this, getData());
+		mPullToRefreshListView.setAdapter(mListAdapter);
+		mPullToRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				// TODO Auto-generated method stub
+				String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),
+						DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+				// Update the LastUpdatedLabel
+				refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+				// Do work to refresh the list here.
+				new GetDataTask().execute();
+			}
+		});
+	}
+	
+	public List<Map<String, Object>> getData(){
+		map = new HashMap<String, Object>();
+		map.put("img", R.drawable.xihu1);
+		map.put("title", "新银盏温泉团体门票");
+		map.put("distance", "143km");
+		map.put("summary", "[惠州等]新银盏温泉团体票1张，10人起订，80余中温泉泡池");
+		map.put("price", "75");
+		map.put("oldprice", "118元");
+		map.put("grade", "4.9分");
+		mList.add(map);
+		
+		map=new HashMap<String, Object>();
+		map.put("img", R.drawable.xihu2);
+		map.put("title", "龙门地派温泉票");
+		map.put("distance", "95.5km");
+		map.put("summary", "[龙门县]门票1张，节假日通用，可免费享用水果、饮料、点心等");
+		map.put("price", "98");
+		map.put("oldprice", "168元");
+		map.put("grade", "4.5分");
+		mList.add(map);
+		
+		map=new HashMap<String, Object>();
+		map.put("img", R.drawable.xihu3);
+		map.put("title", "永记生态园");
+		map.put("distance", "29.0km");
+		map.put("summary", "[平山]观光门票1张，置身田园风光，享受假日好心情");
+		map.put("price", "40");
+		map.put("oldprice", "60元");
+		map.put("grade", "4.3分");
+		mList.add(map);
+						
+		return mList;		
+	}
+	
+	private class GetDataTask extends AsyncTask<Void, Void, Map<String, Object>> {
+
+		@Override
+		protected Map<String, Object> doInBackground(Void... params) {
+			// Simulates a background job.
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+			}
+			map=new HashMap<String, Object>();
+			map.put("img", R.drawable.xihu4);
+			map.put("title", "东江游");
+			map.put("distance", "5.4km");
+			map.put("summary", "[东平]换个角度看我们生活的城市！");
+			map.put("price", "25");
+			map.put("oldprice", "50元");
+			map.put("grade", "4.5分");
+			return map;
+		}
+
+		@Override
+		protected void onPostExecute(Map<String, Object> result) {			
+			mList.add(0, result);
+			mListAdapter.notifyDataSetChanged();
+
+			// Call onRefreshComplete when the list has been refreshed.
+			mPullToRefreshListView.onRefreshComplete();
+
+			super.onPostExecute(result);
+		}
+	}
+
+}
